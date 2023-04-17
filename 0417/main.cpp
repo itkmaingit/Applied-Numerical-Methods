6fac96ef729d3da56f3558efc48cdd74a3008393
itkmaingit <itkmain.git@gmail.com>
2023-04-17 00:39:46 +0000

    0417 completed

---
+++ 0417/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+double f(double x)
+{
+  return exp(x) - (2 - pow(x, 2));
+}
+
+double g(double x)
+{
+  return exp(x) + 2 * x;
+}
+
+int main()
+{
+  double a;
+  double error = 1e-7;
+  std::cin >> a;
+
+  while (true)
+  {
+    std::cout << std::setprecision(7) << std::fixed << a << std::endl;
+    if (abs(f(a)) < error)
+    {
+      std::cout << std::setprecision(7) << std::fixed << a << std::endl;
+      return 0;
+    }
+
+    a = a - f(a) / g(a);
+  }
+}
